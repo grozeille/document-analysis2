59be58c838d0b0ba6b11a9752ac9256136b009d8
mathias kluba <mathias.kluba@gmail.com>
2015-12-28 21:29:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+To build and run
+
+```
+mvn package
+
+set RAW_PATH=C:\Users\Mathias\vagrant\hadoop\document-analysis\input\03_A_ranger
+set RAW_PATH=C:\Users\Mathias\vagrant\hadoop\document-analysis\input\test
+set AVRO_PATH=C:\Users\Mathias\vagrant\hadoop\document-analysis\output\avro
+set PARSED_PATH=C:\Users\Mathias\vagrant\hadoop\document-analysis\output\parsed
+set TESSERACT_PATH=C:\Users\Mathias\Work\Tools\tessdata-master
+set TESSERACT_LANG=fra
+
+java -cp target\scala-2.10\document-analysis-1.0-SNAPSHOT-hadoop2.6.0.jar org.grozeille.CopyToAvro -i %RAW_PATH% -o %AVRO_PATH%
+
+set SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+
+
+
+spark-submit --master local[8] --class org.grozeille.DocumentAnalysis target\scala-2.10\document-analysis-1.0-SNAPSHOT-hadoop2.6.0.jar -i %AVRO_PATH% -o %PARSED_PATH% -t %TESSERACT_PATH%
+```
---
+++ src/main/java/org/grozeille/CopyToAvro.java
@@ -0,0 +1,239 @@
+package org.grozeille;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.Schema;
+import org.apache.avro.file.CodecFactory;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.reflect.ReflectDatumWriter;
+import org.apache.commons.cli.*;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.grozeille.avro.Document;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by Mathias on 27/12/2015.
+ */
+@Slf4j
+public class CopyToAvro {
+
+    private static int cptFile = 0;
+
+    private static String[] blacklist = new String[]{
+      ".DS_Store", "Thumbs.db"
+    };
+
+    public static void main(String[] args) throws Exception {
+        Option inputOption  = OptionBuilder.withArgName( "input" )
+                .isRequired()
+                .hasArgs()
+                .withDescription( "Input path." )
+                .create( "i" );
+        Option outputOption  = OptionBuilder.withArgName( "output" )
+                .isRequired()
+                .hasArgs()
+                .withDescription( "Output path for avro files." )
+                .create( "o" );
+        Option splitSizeOption  = OptionBuilder.withArgName( "split-size" )
+                .hasArgs()
+                .withDescription( "Size of avro files." )
+                .create( "s" );
+
+        Options options = new Options();
+        options.addOption(inputOption);
+        options.addOption(outputOption);
+        options.addOption(splitSizeOption);
+
+        // create the parser
+        CommandLineParser parser = new BasicParser();
+        CommandLine line = null;
+        try {
+            // parse the command line arguments
+            line = parser.parse( options, args );
+        }
+        catch( ParseException exp ) {
+            // oops, something went wrong
+            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp( "CopyToAvro", options );
+
+            System.exit(-1);
+        }
+
+
+        String inputPath = line.getOptionValue("i");
+        String outputPath = line.getOptionValue("o");
+        Long defaultSplitSize =  new Long(1024l*1024l*128l);
+        String splitSizeString = line.getOptionValue("s", defaultSplitSize.toString());
+        Long splitSize = defaultSplitSize;
+        try {
+            splitSize = Long.parseLong(splitSizeString);
+        }catch (Exception ex){
+            log.warn("Unable to parse default size: "+splitSizeString+". Use default value: "+defaultSplitSize);
+        }
+
+        File parentOutputFolder = new File(outputPath);
+        if(parentOutputFolder.exists()){
+            System.err.println("Folder "+outputPath+" already exist");
+            log.error("Folder "+outputPath+" already exist");
+            System.exit(-1);
+        }
+
+        parentOutputFolder.mkdirs();
+
+
+        List<String> pathList = new ArrayList<>();
+        pathList.add(new File(inputPath).toString());
+        pathList.addAll(scanSubfolders(inputPath));
+        log.info("Folders to analyse:");
+        pathList.forEach(log::info);
+
+
+        DataFileWriter<Document> dataFileWriter = createAvroDocumentFile(outputPath+"/result"+String.format("%03d", cptFile)+".avro");
+
+        try {
+
+
+            // list all files to write in avro
+            for (String path : pathList) {
+
+                File pathFile = new File(path);
+                File[] files = pathFile.listFiles();
+                if(files == null){
+                    continue;
+                }
+
+                for (File child : files) {
+                    if (child.isFile()) {
+
+                        if(isBlacklisted(child)){
+                            continue;
+                        }
+
+                        String extension = FilenameUtils.getExtension(child.getAbsolutePath());
+
+                        if ("zip".equalsIgnoreCase(extension)) {
+
+                            try (InputStream stream = new FileInputStream(child)) {
+                                ZipInputStream zis = new ZipInputStream(stream);
+
+                                scanZipArchive(dataFileWriter, child.getAbsolutePath(), zis, outputPath, splitSize);
+                            }
+                        } else {
+                            Document document = toDocument(child);
+                            dataFileWriter.append(document);
+
+                            if(dataFileWriter.sync() >= splitSize){
+                                dataFileWriter.close();
+                                cptFile++;
+                                dataFileWriter = createAvroDocumentFile(outputPath+"/result"+String.format("%03d", cptFile)+".avro");
+                            }
+                        }
+                    }
+                }
+            }
+        }finally {
+            dataFileWriter.close();
+        }
+    }
+
+    private static DataFileWriter<Document> scanZipArchive(DataFileWriter<Document> dataFileWriter, String parentPath, ZipInputStream zis, String outputPath, long splitSize) throws IOException {
+
+
+        System.out.println("Scan zip file: "+parentPath);
+        try {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (!entry.isDirectory()) {
+
+                    String entryPath = parentPath + "/" + entry.getName();
+
+                    try {
+                        if ("zip".equalsIgnoreCase(FilenameUtils.getExtension(entry.getName()))) {
+                            ZipInputStream subZis = new ZipInputStream(zis);
+                            dataFileWriter = scanZipArchive(dataFileWriter, entryPath, subZis, outputPath, splitSize);
+                        } else {
+                            Document document = toDocument(entryPath, zis);
+                            dataFileWriter.append(document);
+                            if (dataFileWriter.sync() >= splitSize) {
+                                dataFileWriter.close();
+                                cptFile++;
+                                dataFileWriter = createAvroDocumentFile(outputPath + "/result" + String.format("%03d", cptFile) + ".avro");
+                            }
+                        }
+                    }finally {
+                        zis.closeEntry();
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            log.error("Unable to read zip file " + parentPath, ex);
+        }
+
+        return dataFileWriter;
+    }
+
+    private static boolean isBlacklisted(File file){
+        for(String b : blacklist){
+            if(b.equalsIgnoreCase(file.getName())){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static DataFileWriter<Document> createAvroDocumentFile(String outputPath) throws IOException {
+        Schema schema = ReflectData.get().getSchema(Document.class);
+        File file = new File(outputPath);
+        DatumWriter<Document> writer = new ReflectDatumWriter<>(Document.class);
+        return new DataFileWriter<>(writer)
+                .setCodec(CodecFactory.snappyCodec())
+                .create(schema, file);
+    }
+
+    private static Document toDocument(String path, InputStream stream) throws IOException {
+        Document doc = new Document();
+        doc.setPath(path);
+        doc.setLang("");
+        ByteArrayOutputStream ous = new ByteArrayOutputStream();
+        IOUtils.copy(stream, ous);
+
+        return doc;
+    }
+
+    private static Document toDocument(File file) throws IOException {
+        try(InputStream stream = new FileInputStream(file)) {
+            return toDocument(file.getAbsolutePath(), stream);
+        }
+    }
+
+    private static Collection<? extends String> scanSubfolders(String inputPath) {
+        File parent = new File(inputPath);
+        List<String> folders = new ArrayList<>();
+        String[] directories = parent.list((current, name) -> new File(current, name).isDirectory());
+        if(directories != null) {
+            for (String d : directories) {
+                folders.add(new File(parent, d).toString());
+                folders.addAll(scanSubfolders(new File(parent, d).getAbsolutePath()));
+            }
+        }
+        return folders;
+    }
+}
